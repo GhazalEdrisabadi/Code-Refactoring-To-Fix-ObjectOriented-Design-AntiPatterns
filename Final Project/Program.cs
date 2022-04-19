a7193570da6365958882204b7052d01fb7003a2b
Ghazaal <ghazal_edrisabadi@comp.iust.ac.ir>
2022-04-19 11:00:27 +0430

    Add antlr files

--- Final Project/Program.cs
+++ Final Project/Program.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Antlr4.Runtime;
+using Antlr4.Runtime.Tree;
+using System.IO;
 
 namespace Final_Project
 {
@@ -9,6 +12,17 @@ namespace Final_Project
     {
         static void Main(string[] args)
         {
+            StreamWriter w1 = new StreamWriter(@"C:\Javalib\test.txt");
+            StreamWriter w2 = new StreamWriter(@"C:\Javalib\error.txt");
+            String input = "int";
+            ICharStream stream2 = new AntlrInputStream(input);
+            ITokenSource lexer = new CSharpLexer(stream2);
+            ITokenStream tokens = new CommonTokenStream(lexer);
+            CSharpParser parser = new CSharpParser(tokens);
+            parser.BuildParseTree = true;
+            CSharpParser.LiteralContext result = parser.literal();
+            Console.Write(result.ToStringTree());
+            Console.ReadKey();
         }
     }
 }
